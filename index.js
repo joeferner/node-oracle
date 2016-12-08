d2295ddc66cf713ad53d7240d5e5866a423283b6
Joe Ferner <joe@fernsroth.com>
2016-12-07 21:52:56 -0500

    deprecate product

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 
-module.exports = require('./lib/oracle');
+console.error("This library is not maintained.  Oracle has made their own [driver](https://github.com/oracle/node-oracledb).  Please install the new driver with 'npm install oracledb'");
+console.error("If you absolutely need the old driver, use 'npm install oracle@0.3.9'")
