9f1e9b10046524a904b4eef713032fdbed20eddf
Joe Ferner <joe@fernsroth.com>
2016-12-20 18:27:18 -0500

    fix install

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# This library is not maintained.  Oracle has a [new oracledb driver](https://github.com/oracle/node-oracledb).  Please install the new driver with 'npm install oracledb'
+# This library is not maintained.
+
+The node-oracle driver is obsolete.  Oracle has a new
+driver: [node-oracledb](https://github.com/oracle/node-oracledb).
+
+Please install the new driver with 'npm install oracledb'.  Instructions are [here](https://github.com/oracle/node-oracledb/blob/master/INSTALL.md).
 
 If you absolutely need the old driver, use 'npm install oracle@0.3.9'
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 
-console.error("This library is not maintained.  Oracle has made their own [driver](https://github.com/oracle/node-oracledb).  Please install the new driver with 'npm install oracledb'");
-console.error("If you absolutely need the old driver, use 'npm install oracle@0.3.9'")
+console.error("This node-oracle driver is obsolete.  Oracle has a new driver: https://github.com/oracle/node-oracledb.");
+console.error("Please install the new driver with 'npm install oracledb', see https://github.com/oracle/node-oracledb/blob/master/INSTALL.md");
+console.error("If you absolutely need the old driver, use 'npm install oracle@0.3.9'");
